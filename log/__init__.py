296c48cf83b1e8baf932996ca1adb70068c84b6c
vincecrit <vince.crit@gmail.com>
2025-03-18 16:18:12 +0100

    Correzione moduli log e ot.interfaces

--- log/__init__.py
+++ log/__init__.py
@@ -5,11 +5,6 @@ from logging.handlers import TimedRotatingFileHandler
 LOG_FORMAT = "%(asctime)s [%(name)s.%(funcName)s - %(lineno)s][%(levelname)s] - %(message)s"
 LOG_LEVEL = logging.DEBUG  # Puoi cambiarlo a INFO, WARNING, ERROR, CRITICAL
 
-LOG_FILE = "wp32.log"
-LOG_DIR = "logs"
-
-os.makedirs(LOG_DIR, exist_ok=True)
-log_path = os.path.join(LOG_DIR, LOG_FILE)
 
 def setup_logger(name: str):
     logger = logging.getLogger(name)
@@ -27,7 +22,8 @@ def setup_logger(name: str):
     logger.addHandler(console_handler)
     
     # Handler per il file (a tempo, un file ogni giorno)
-    file_handler = TimedRotatingFileHandler(log_path, when='d', interval=1)
+    file_handler = TimedRotatingFileHandler(filename="log.txt", when='D', interval=1)
+    file_handler.suffix = "%Y%m%d"
     file_handler.setLevel(LOG_LEVEL)
     file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
     logger.addHandler(file_handler)
